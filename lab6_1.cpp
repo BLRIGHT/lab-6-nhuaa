107003d2e57d5914e1a51f434c6b70ab49681c0e
BLRIGHT <naluephonkt1249@gmail.com>
2024-11-29 17:15:55 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+
+int N=1;
+int i=0;
+int odd=0;
+int even=0; 
+
+while(N!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin  >> N;
+    if (N!=0){
+    if(N%2==0){
+        even+=1;
+    }
+    else{
+        odd+=1;
+    }
+}
+}
+
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd <<"\n" ;
+    
 }
